0497c5dfdfeea8d95e4a54da464319cc5cdc9300
marinaabdulova25 <marinaabdulova25@mail.ru>
2025-10-24 20:02:16 +0300

    Implement factorial calculation in C

--- task2/Marina_hw_task2.c
+++ task2/Marina_hw_task2.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+long long factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int number;
+    
+    printf("Введите число для вычисления факториала: ");
+    scanf("%d", &number);
+    
+    if (number < 0) {
+        printf("Ошибка: Факториал отрицательного числа не определен.\n");
+    } else {
+        long long result = factorial(number);
+        printf("%d! = %lld\n", number, result);
+    }
+    
+    return 0;
+}
